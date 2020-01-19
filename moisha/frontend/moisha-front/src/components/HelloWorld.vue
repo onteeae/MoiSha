c9a7a4de659c989793bd0fa70109f8f03c027955
jaeyoon-lee <jaeyoon.leee@webtoonscorp.com>
2020-01-19 16:03:46 +0900

    .gitignore에서 build 제거

--- moisha/frontend/moisha-front/src/components/HelloWorld.vue
+++ moisha/frontend/moisha-front/src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <h2>안녕</h2>
     <ul>
       <li>
         <a
@@ -88,7 +88,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Moisha'
     }
   }
 }
